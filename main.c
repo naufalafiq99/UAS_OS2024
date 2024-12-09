3fbc8ecb11b281da1fa8361a069728effa6060a0
naufalafiq99 <afiq.naufal99r@gmail.com>
2024-12-09 17:22:12 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "fall.h"
+
+int main ()
+{
+
+  printf("\t\t Library sistem operasi\n");
+  printf("\n ============================== \n");
+  printf("Welcome to converter money");
+  printf("\n ============================== \n");
+  printf("\n nilai dolar ke rupiah ");
+  rupiah();
+  
+  printf("\n nilai rupiah ke dolar \n");
+  dolar();
+}
